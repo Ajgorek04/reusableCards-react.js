5de8ef8e36876a318c050027140c97ffbec750d5
Ajgorek <igorblendzinski12@gmail.com>
2023-10-29 21:11:17 +0100

    Creating list with objects and mapping them

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,57 @@
 import "./App.css";
 import { Card } from "./Components/Card/Card";
-import cryptoImg from "./assets/cryptoImg.jpg";
+import bitcoin from "./assets/bitcoin.jpg";
+import ethereum from "./assets/ethereum.jpg";
+import litecoin from "./assets/litecoin.jpg";
 
 function App() {
+    const cryptocurrencies = [
+        {
+            name: "Bitcoin (BTC)",
+            img: bitcoin,
+            text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit fugiat deserunt odio possimus accusamus veritatis nam, culpa veniam. Suscipit fugiat ipsam nobis! Optio, voluptatibus odit",
+            link: "https://coinmarketcap.com/currencies/bitcoin/",
+        },
+        {
+            name: "Ethereum (ETH)",
+            img: ethereum,
+            text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio iure necessitatibus quae molestiae non hic sit veritatis ex facilis unde",
+            link: "https://coinmarketcap.com/currencies/ethereum/",
+        },
+        {
+            name: "Litecoin (LTC)",
+            img: litecoin,
+            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit doloremque provident laboriosam incidunt voluptatibus minima?",
+            link: "https://coinmarketcap.com/currencies/litecoin/",
+        },
+    ];
+
     return (
         <div className="container">
             <div className="card">
                 <header>
                     <h1>Latest Crypto News</h1>
                 </header>
-                <div className="imageText">
-                    <img src={cryptoImg} alt="cryptoImage" />
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur, adipisicing
-                        elit. Illo nesciunt qui accusantium sint animi quos at,
-                        accusamus suscipit. Voluptate, optio.
-                    </p>
-                </div>
-                <div className="readMore">
-                    <a href="#" target="_blank" rel="noopener noreferrer">
-                        Click here for more Info
+                <div className="designedBy">
+                    <h2>Designed by</h2>
+                    <a
+                        href="https://github.com/Ajgorek04"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Ajgorek04
                     </a>
                 </div>
             </div>
-            <Card cardName="Bitcoin (BTC)" />
-            <Card cardName="Etherum (ETH)" />
-            <Card cardName="Litecoin (LTC)" />
+            {cryptocurrencies.map((crypto, index) => (
+                <Card
+                    key={index}
+                    name={crypto.name}
+                    img={crypto.img}
+                    text={crypto.text}
+                    link={crypto.link}
+                />
+            ))}
         </div>
     );
 }
--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,9 +1,23 @@
-export function Card({ cardName }) {
+import styles from "./Card.module.css";
+
+export function Card({ name, img, text, link }) {
     return (
-        <div className="card">
+        <div className={styles.card}>
             <header>
-                <h1>{cardName}</h1>
+                <img src={img} alt="image" />
+                <p>September 10.2023</p>
             </header>
+            <div className={styles.textContent}>
+                <h1>{name}</h1>
+                <p>{text}</p>
+            </div>
+            <div className={styles.learnMore}>
+                <h2>
+                    <a href={link} target="_blank" rel="noopener noreferrer">
+                        Learn More
+                    </a>
+                </h2>
+            </div>
         </div>
     );
 }
