19c9b937f50fa0cccb14b1544b48834a5e084296
Ajgorek <igorblendzinski12@gmail.com>
2023-10-29 20:18:45 +0100

    Creating first no reusable card

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,33 @@
 import "./App.css";
+import { Card } from "./Components/Card/Card";
+import cryptoImg from "./assets/cryptoImg.jpg";
 
-function App() {}
+function App() {
+    return (
+        <div className="container">
+            <div className="card">
+                <header>
+                    <h1>Latest Crypto News</h1>
+                </header>
+                <div className="imageText">
+                    <img src={cryptoImg} alt="cryptoImage" />
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing
+                        elit. Illo nesciunt qui accusantium sint animi quos at,
+                        accusamus suscipit. Voluptate, optio.
+                    </p>
+                </div>
+                <div className="readMore">
+                    <a href="#" target="_blank" rel="noopener noreferrer">
+                        Click here for more Info
+                    </a>
+                </div>
+            </div>
+            <Card cardName="Bitcoin (BTC)" />
+            <Card cardName="Etherum (ETH)" />
+            <Card cardName="Litecoin (LTC)" />
+        </div>
+    );
+}
 
 export default App;
---
+++ src/Components/Card/Card.jsx
@@ -0,0 +1,9 @@
+export function Card({ cardName }) {
+    return (
+        <div className="card">
+            <header>
+                <h1>{cardName}</h1>
+            </header>
+        </div>
+    );
+}
